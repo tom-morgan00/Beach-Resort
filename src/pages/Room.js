ac90de5e550ebef4f42db893f3e13de83264b0af
Tom Morgan <thomas.morgan1903@gmail.com>
2021-04-05 11:11:45 +0100

    room page

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -4,6 +4,7 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../context';
+import StyledHero from '../components/StyledHero';
 
 export default class Room extends Component {
   state = {
@@ -32,20 +33,51 @@ export default class Room extends Component {
       description,
       capacity,
       size,
-      prize,
+      price,
       extras,
       breakfast,
       pets,
       images,
     } = room;
+    const [mainImg, ...defaultImg] = images;
     return (
-      <Hero hero={'roomsHero'}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </Hero>
+      <>
+        <StyledHero image={mainImg || defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((img, i) => {
+              return <img src={img} key={i} alt={name} />;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>Price: £{price}</h6>
+              <h6>Size: {size} SQ FT</h6>
+              <h6>Beds: {capacity}</h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+            </article>
+          </div>
+          <section className="room-extras">
+            <h6>extras</h6>
+            <ul className="extras">
+              {extras.map((extra, i) => {
+                return <li key={i}>{extra}</li>;
+              })}
+            </ul>
+          </section>
+        </section>
+      </>
     );
   }
 }
